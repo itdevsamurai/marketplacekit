bd368687fee43db8977c2581dfd6c62f87c4dc98
root <root@nb101-marketplace-vm.c.tam-seo.internal>
2018-07-25 01:35:51 +0000

    Add user verification email templates

---
+++ resources/lang/vendor/laravel-user-verification/vi/user-verification.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel User Verification Language Pack
+    |--------------------------------------------------------------------------
+    */
+
+    'verification_email_subject' => 'Xac thuc tai khoan',
+
+    // error view
+    'verification_error_header' => 'Xac thuc loi',
+    'verification_error_message' => 'Kho xac thuc duoc tai khoan cua ban.',
+    'verification_error_back_button' => 'Tro lai',
+
+];
---
+++ resources/lang/vi/user-verification.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel User Verification Language Pack
+    |--------------------------------------------------------------------------
+    */
+    'verification_email_subject' => 'Xác nhận tài khoản và địa chỉ email',
+    // error view
+    'verification_error_header' => 'Xác nhận lỗi',
+    'verification_error_message' => 'Tài khoản của bạn không được xác nhận.',
+    'verification_error_back_button' => 'Trở lại',
+];
