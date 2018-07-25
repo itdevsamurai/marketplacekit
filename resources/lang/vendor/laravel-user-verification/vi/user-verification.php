6e43f033f1df7dfa0aa291ad04d50f1406f74880
Tam Nguyen <tamseo@gmail.com>
2018-07-25 10:59:35 +0900

    Add user verification translation

--- resources/lang/vendor/laravel-user-verification/vi/user-verification.php
+++ resources/lang/vendor/laravel-user-verification/vi/user-verification.php
@@ -1,18 +1,13 @@
 <?php
-
 return [
-
     /*
     |--------------------------------------------------------------------------
     | Laravel User Verification Language Pack
     |--------------------------------------------------------------------------
     */
-
-    'verification_email_subject' => 'Xac thuc tai khoan',
-
+    'verification_email_subject' => 'Xác nhận tài khoản và địa chỉ email',
     // error view
-    'verification_error_header' => 'Xac thuc loi',
-    'verification_error_message' => 'Kho xac thuc duoc tai khoan cua ban.',
-    'verification_error_back_button' => 'Tro lai',
-
+    'verification_error_header' => 'Xác nhận lỗi',
+    'verification_error_message' => 'Tài khoản của bạn không được xác nhận.',
+    'verification_error_back_button' => 'Trở lại',
 ];
